640af2bce67ee167d11bdc4998e7897687a047db
Mariusz Nowak <mariusz@medikoo.com>
2012-10-11 23:12:24 +0200

    HTMLDocument#addStyle

---
+++ test/HTMLDocument/prototype/add-style.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var last = require('es5-ext/lib/Array/prototype/last')
+
+module.exports = function (t, a) {
+	var el1, style;
+	if (typeof document === 'undefined') return;
+
+	t.call(document, 'p { color: red; }');
+
+	style = last.call(document.childNodes[0].getElementsByTagName('style'));
+	a(style.textContent, 'p { color: red; }', "String");
+
+	t.call(document,
+		{ form: { padding: '10px 10px', backgroundColor: 'olive' } });
+
+	a((new RegExp('form\\s*{\\s*padding\\s*:\\s*10px 10px\\s*;\\s*background-'
+		+ 'color\\s*:\\s*olive\\s*;\\s*}')).test(style.textContent), true,
+		"Object");
+};
