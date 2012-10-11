b79668a5a7c573db4feefe1b681fdcc56a7d264c
Mariusz Nowak <mariusz@medikoo.com>
2012-10-11 15:15:21 +0200

    Element#replaceContent

---
+++ test/Element/prototype/replace-content.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var toArray = require('es5-ext/lib/Array/from');
+
+module.exports = function (t, a) {
+	var el1;
+	if (typeof document === 'undefined') return;
+
+	el1 = document.createElement('p');
+	el1.appendChild(document.createElement('span'));
+	el1.appendChild(document.createElement('span'));
+
+	var els = [document.createElement('b'), document.createElement('b')];
+
+	a(t.apply(el1, els), el1, "Return");
+	a.deep(toArray(el1.childNodes), els, "Content");
+};
