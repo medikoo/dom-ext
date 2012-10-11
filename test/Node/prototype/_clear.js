4f074988c96c253f112a6eb4c2c47f79e191ac98
Mariusz Nowak <mariusz@medikoo.com>
2012-10-11 14:53:47 +0200

    Node#_clear

---
+++ test/Node/prototype/_clear.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var el;
+
+	if (typeof document === 'undefined') return;
+
+	el = document.createElement('p');
+	el.appendChild(document.createElement('span'));
+	el.appendChild(document.createElement('span'));
+
+	a(t.call(el), el, "Returns self");
+	a(el.childNodes.length, 0, "Cleared");
+	t.call(el);
+	a(el.childNodes.length, 0, "Cleared on empty");
+};
