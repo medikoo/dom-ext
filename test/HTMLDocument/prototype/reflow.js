f3a4b7b9417576c404647cab8619d26c0a1b15af
Mariusz Nowak <mariusz@medikoo.com>
2012-10-12 16:29:54 +0200

    HTMLDocument#reflow

---
+++ test/HTMLDocument/prototype/reflow.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (t, a) {
+	if ((typeof document === 'undefined') || !document.body.classList) return;
+
+	var pre = document.body.className;
+	t.call(document);
+	a.not(pre, document.body.className);
+};
+
