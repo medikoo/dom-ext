a1e0263a67cbd1c10bc8d34f69057f0d86cb23aa
Mariusz Nowak <mariusz@medikoo.com>
2013-01-24 22:29:46 +0100

    Element#append

---
+++ test/Element/prototype/append.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var toArray = require('es5-ext/lib/Array/from');
+
+module.exports = function (t, a) {
+	var el, prechild, child, text;
+	if (typeof document === 'undefined') return;
+
+	el = document.createElement('p');
+	prechild = el.appendChild(document.createElement('span'));
+
+	t.call(el, 'raz', child = document.createElement('span'));
+	text = el.childNodes[1];
+	a(text.nodeType, 3, "String: Type");
+	a.deep(toArray(el.childNodes), [prechild, text, child]);
+};
