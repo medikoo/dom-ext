ef0ad4cf6af5c80cbfec02985f7f853961a6749a
Mariusz Nowak <mariusz@medikoo.com>
2013-01-24 16:12:34 +0100

    Basic function and method for Text
    Specifcally:
    Text.isText
    Text.validText
    Text#exclude
    Text#include
    Text#remove

---
+++ test/Text/prototype/exclude.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var node;
+
+	if (typeof document === 'undefined') return;
+
+	node = document.createElement('p')
+		.appendChild(document.createTextNode('foo'));
+
+	t.call(node); // Nothing happens
+	a(node.parentNode, null, "Removed");
+};
---
+++ test/Text/valid-text.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var node;
+	a.throws(function () { t({}); }, "Other");
+	if (typeof document !== 'undefined') {
+		node = document.createTextNode('');
+		a(t(node), node, "Text node");
+	}
+};
