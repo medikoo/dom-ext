a0c721f519d55bdfb5f7d11e0b0a4cf893d8e2a7
Mariusz Nowak <mariusz@medikoo.com>
2013-01-24 13:42:41 +0100

    Element#exclude and Element#include

---
+++ test/Node/prototype/_exclude.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var el;
+
+	if (typeof document === 'undefined') return;
+
+	el = document.createElement('p').appendChild(document.createTextNode(''));
+
+	t.call(el); // Nothing happens
+	a(el.parentNode, null, "Removed");
+};
