2597091b405b4681607e12af5b7dc93e0231d561
Mariusz Nowak <mariusz@medikoo.com>
2013-01-24 19:38:02 +0100

    Node.validNode

---
+++ test/Node/valid-node.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var node;
+	a.throws(function () { t({}); }, "Other");
+	if (typeof document !== 'undefined') {
+		node = document.createTextNode('');
+		a(t(node), node, "Text");
+		node = document.createComment('');
+		a(t(node), node, "Text Comment");
+		node = document.createElement('div');
+		a(t(node), node, "Element");
+	}
+};
