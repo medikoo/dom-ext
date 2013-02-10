bb27224b53d2d3d976ab95e3c8e387232be45b37
Mariusz Nowak <mariusz@medikoo.com>
2013-02-10 17:18:50 +0100

    Attribute is not a Node (as per DOM4 spec)

--- test/Node/is-node.js
+++ test/Node/is-node.js
@@ -5,11 +5,12 @@ module.exports = function (t, a) {
 	a(t("test"), false, "Primitive");
 	a(t({}), false, "Any object");
 
-	if (typeof document !== 'undefined') {
-		a(t(document.createDocumentFragment()), true, "DocumentFragment");
-		a(t(document.createElement('div')), true, "DocumentFragment");
-		a(t(document.createTextNode('content')), true, "Text node");
-		a(t(document.createComment('content')), true, "Comment node");
-		a(t(document), true, "Document node");
-	}
+	if (typeof document === 'undefined') return;
+
+	a(t(document.createDocumentFragment()), true, "DocumentFragment");
+	a(t(document.createElement('div')), true, "DocumentFragment");
+	a(t(document.createTextNode('content')), true, "Text node");
+	a(t(document.createComment('content')), true, "Comment node");
+	a(t(document), true, "Document node");
+	a(t(document.createAttribute('raz')), false, "Attribute");
 };
