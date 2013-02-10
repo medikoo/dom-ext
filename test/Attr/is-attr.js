a0292c83e878cafc4c7bf43a37ad8f370a6926d5
Mariusz Nowak <mariusz@medikoo.com>
2013-02-10 17:33:38 +0100

    Attr.isAttr

---
+++ test/Attr/is-attr.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function (t, a) {
+	a(t(null), false, "Null");
+	a(t("test"), false, "Primitive");
+	a(t({}), false, "Any object");
+
+	if (typeof document === 'undefined') return;
+
+	a(t(document.createDocumentFragment()), false, "DocumentFragment");
+	a(t(document.createElement('div')), false, "DocumentFragment");
+	a(t(document.createTextNode('content')), false, "Text node");
+	a(t(document.createComment('content')), false, "Comment node");
+	a(t(document), false, "Document node");
+	a(t(document.createAttribute('raz')), true, "Attribute");
+};
