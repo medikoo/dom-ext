73b40fa5ec535b6cc1ba04b73f5a1c8013ed7ee7
Mariusz Nowak <mariusz@medikoo.com>
2013-03-20 14:09:47 +0100

    Basic functions for HTMLTableRowElement

---
+++ test/HTMLTableRowElement/valid-html-table-row-element.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var el;
+	a.throws(function () {
+		t({});
+	}, "Other");
+	if (typeof document !== 'undefined') {
+		el = document.createElement('tr');
+		a(t(el), el, "TableRow");
+	}
+};
