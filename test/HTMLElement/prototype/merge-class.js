58fa5cbe8e259b5afac4dada3dab3c7626bf636f
Mariusz Nowak <mariusz@medikoo.com>
2013-05-03 14:58:57 +0200

    HTMLElement#mergeClass

---
+++ test/HTMLElement/prototype/merge-class.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var el;
+
+	if (typeof document === 'undefined') return;
+
+	el = document.createElement('p');
+	a(t.call(el, 'raz dwa').className, 'raz dwa', "On empty");
+	a(t.call(el, 'trzy   cztery  ').className, 'raz dwa trzy cztery',
+		"On Existing");
+	a(t.call(el, 'trzy pięć raz  ').className, 'raz dwa trzy cztery pięć',
+		"On overlap");
+};
