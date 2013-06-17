f7a478b3e02e7951125b7df92a57ebbc807c2a3d
Mariusz Nowak <mariusz@medikoo.com>
2013-06-17 22:13:06 +0200

    Node#remove

---
+++ test/Node/prototype/remove.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var el;
+
+	if (typeof document === 'undefined') return;
+
+	el = document.createElement('p');
+	t.call(el.appendChild(document.createElement('span')));
+	a(el.childNodes.length, 0);
+	t.call(el.appendChild(document.createTextNode('')));
+	a(el.childNodes.length, 0);
+};
