47be4405cc3cccdfe7bafa48dc947f72e4610ce1
Mariusz Nowak <mariusz@medikoo.com>
2014-08-29 17:41:42 +0200

    Revert change that bring location node awareness

--- node/#/_replace-content.js
+++ node/#/_replace-content.js
@@ -5,36 +5,22 @@ var normalize = require('../../document/#/normalize')
   , isArray = Array.isArray;
 
 module.exports = function (child/*, …child*/) {
-	var oldNodes = this.childNodes, nuNodes, nu, i = 0, l, old;
+	var oldNodes = this.childNodes, nuNodes, nu, i = 0, old;
 	nuNodes = normalize.apply(this.ownerDocument, arguments);
 
-	if (nuNodes) {
-		if (isArray(nuNodes)) {
-			for (i = 0, l = nuNodes.length; i < l; ++i) {
-				if (nuNodes[i]._domExtLocation) {
-					nuNodes.splice(i + 1, 0, nuNodes[i]._domExtLocation);
-					++i;
-					++l;
-				}
-			}
-			i = 0;
-		} else if (nuNodes._domExtLocation) {
-			nuNodes = [nuNodes, nuNodes._domExtLocation];
-		}
-		if (isArray(nuNodes)) {
-			while ((nu = nuNodes.shift())) {
-				old = oldNodes[i++];
-				if (old !== nu) {
-					if (old) this.replaceChild(nu, old);
-					else this.appendChild(nu);
-				}
-			}
-		} else if (nuNodes) {
+	if (isArray(nuNodes)) {
+		while ((nu = nuNodes.shift())) {
 			old = oldNodes[i++];
-			if (old !== nuNodes) {
-				if (old) this.replaceChild(nuNodes, old);
-				else this.appendChild(nuNodes);
+			if (old !== nu) {
+				if (old) this.replaceChild(nu, old);
+				else this.appendChild(nu);
 			}
+		}
+	} else if (nuNodes) {
+		old = oldNodes[i++];
+		if (old !== nuNodes) {
+			if (old) this.replaceChild(nuNodes, old);
+			else this.appendChild(nuNodes);
 		}
 	}
 	while (oldNodes.length > i) this.removeChild(oldNodes[i]);
