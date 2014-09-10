3495a1b074d0baaae3d8a0f66f5e94d13fae2482
Mariusz Nowak <mariusz@medikoo.com>
2014-09-10 14:32:03 +0200

    Allow conditional synchronization

--- html-element/#/sync-style.js
+++ html-element/#/sync-style.js
@@ -1,20 +1,27 @@
 'use strict';
 
-var value           = require('es5-ext/object/valid-value')
-  , once            = require('timers-ext/once')
-  , element         = require('../valid-html-element')
+var callable = require('es5-ext/object/valid-callable')
+  , value    = require('es5-ext/object/valid-value')
+  , once     = require('timers-ext/once')
+  , element  = require('../valid-html-element')
 
   , observerConfig = { attributes: true, childList: true, subtree: true };
 
-module.exports = function (source, propertyName) {
-	var update, computedStyle, sourceWindow, target, mutationObserver, interval;
+module.exports = function (source, propertyName/*, isApplicable*/) {
+	var update, computedStyle, sourceWindow, target, mutationObserver, interval
+	  , initial, isApplicable = arguments[2];
 
 	target = element(this);
 	element(source);
 	propertyName = String(value(propertyName));
+	if (isApplicable !== undefined) callable(isApplicable);
 	sourceWindow = source.ownerDocument.defaultView;
 	computedStyle = sourceWindow.getComputedStyle(source);
-	update = once(function () { target.style[propertyName] = computedStyle[propertyName]; });
+	initial = target.style[propertyName];
+	update = once(function () {
+		target.style[propertyName] = (isApplicable && !isApplicable())
+			? initial : computedStyle[propertyName];
+	});
 
 	// on resize
 	sourceWindow.addEventListener('resize', update, false);
