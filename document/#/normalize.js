3a335d561801cba820a8b9a7a6a2da0881d91178
Mariusz Nowak <medyk@medikoo.com>
2014-10-24 14:28:22 +0200

    Fix handling of documentFragments

--- document/#/normalize.js
+++ document/#/normalize.js
@@ -1,14 +1,15 @@
 'use strict';
 
-var isCallable  = require('es5-ext/object/is-callable')
-  , isArrayLike = require('es5-ext/object/is-array-like')
-  , isObject    = require('es5-ext/object/is-object')
-  , isString    = require('es5-ext/string/is-string')
-  , isIterable  = require('es6-iterator/is-iterable')
-  , forOf       = require('es6-iterator/for-of')
-  , isAttr      = require('../../attr/is-attr')
-  , isNode      = require('../../node/is-node')
-  , document    = require('../valid-document')
+var isCallable         = require('es5-ext/object/is-callable')
+  , isArrayLike        = require('es5-ext/object/is-array-like')
+  , isObject           = require('es5-ext/object/is-object')
+  , isString           = require('es5-ext/string/is-string')
+  , isIterable         = require('es6-iterator/is-iterable')
+  , forOf              = require('es6-iterator/for-of')
+  , isDocumentFragment = require('../../document-fragment/is-document-fragment')
+  , isAttr             = require('../../attr/is-attr')
+  , isNode             = require('../../node/is-node')
+  , document           = require('../valid-document')
 
   , forEach = Array.prototype.forEach, singular = null, multi = null, normalize;
 
@@ -18,25 +19,28 @@ normalize = function (child) {
 	if (!isNode(child)) {
 		if (isAttr(child)) throw new TypeError("Free pass of attribute nodes is not supported");
 		if (child.toDOM && isCallable(child.toDOM)) {
-			child = child.toDOM(this);
-		} else {
-			if (isObject(child)) {
-				if (isIterable(child)) {
-					if (child.forEach) {
-						child.forEach(normalize, this);
-						return;
-					}
-					if (!isString(child)) {
-						forOf(child, normalize, this);
-						return;
-					}
-				} else if (isArrayLike(child)) {
-					forEach.call(child, normalize, this);
+			normalize(child.toDOM(this));
+			return;
+		}
+		if (isObject(child)) {
+			if (isIterable(child)) {
+				if (child.forEach) {
+					child.forEach(normalize, this);
 					return;
 				}
+				if (!isString(child)) {
+					forOf(child, normalize, this);
+					return;
+				}
+			} else if (isArrayLike(child)) {
+				forEach.call(child, normalize, this);
+				return;
 			}
-			child = this.createTextNode(String(child));
 		}
+		child = this.createTextNode(String(child));
+	} else if (isDocumentFragment(child)) {
+		forEach.call(child.childNodes, normalize, this);
+		return;
 	}
 	if (multi) {
 		index = multi.indexOf(child);
