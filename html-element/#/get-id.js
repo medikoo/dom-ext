42e31e6b518b6f53ef635b008b1aa65a2933fb12
Mariusz Nowak <medyk@medikoo.com>
2014-11-12 12:56:27 +0100

    Use plain random generator instead of time based

--- html-element/#/get-id.js
+++ html-element/#/get-id.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var element  = require('../valid-html-element')
-  , randomId = require('time-uuid');
+var element    = require('../valid-html-element')
+  , randomUniq = require('time-uuid');
 
 module.exports = function () {
 	if (element(this).id) return this.id;
-	return (this.id = this.nodeName.toLowerCase() + '-' + randomId());
+	return (this.id = this.nodeName.toLowerCase() + '-' + randomUniq());
 };
