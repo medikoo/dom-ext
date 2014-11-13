3603b6477b7e21ad5c106a5a3c89abfa922c7f6e
Mariusz Nowak <medyk@medikoo.com>
2014-11-13 15:59:38 +0100

    Rename variable

--- html-element/is-block-element.js
+++ html-element/is-block-element.js
@@ -3,11 +3,11 @@
 var primitiveSet = require('es5-ext/object/primitive-set')
   , isElement    = require('./is-html-element');
 
-var blockElementsMap = primitiveSet('address', 'article', 'aside', 'audio', 'blockquote', 'canvas',
-	'dd', 'div', 'dl', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4',
-	'h5', 'h6', 'header', 'hgroup', 'hr', 'noscript', 'ol', 'output', 'p', 'pre', 'section', 'table',
+var namesMap = primitiveSet('address', 'article', 'aside', 'audio', 'blockquote', 'canvas', 'dd',
+	'div', 'dl', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5',
+	'h6', 'header', 'hgroup', 'hr', 'noscript', 'ol', 'output', 'p', 'pre', 'section', 'table',
 	'tfoot', 'ul', 'video');
 
 module.exports = function (el) {
-	return Boolean(isElement(el) && blockElementsMap[el.nodeName.toLowerCase()]);
+	return Boolean(isElement(el) && namesMap[el.nodeName.toLowerCase()]);
 };
