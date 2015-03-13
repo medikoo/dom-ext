7847e81b6901e82e5fac4495780717855f777c7b
Mariusz Nowak <medyk@medikoo.com>
2015-03-13 11:51:49 +0100

    Rename variable

--- node/#/_replace-content.js
+++ node/#/_replace-content.js
@@ -5,24 +5,24 @@ var normalize = require('../../document/#/normalize')
   , isArray = Array.isArray;
 
 module.exports = function (child/*, …child*/) {
-	var oldNodes = this.childNodes, nuNodes, nu, i = 0, old;
+	var nodes = this.childNodes, nuNodes, nu, i = 0, old;
 	nuNodes = normalize.apply(this.ownerDocument, arguments);
 
 	if (isArray(nuNodes)) {
 		while ((nu = nuNodes.shift())) {
-			old = oldNodes[i++];
+			old = nodes[i++];
 			if (old !== nu) {
 				if (old) this.replaceChild(nu, old);
 				else this.appendChild(nu);
 			}
 		}
 	} else if (nuNodes) {
-		old = oldNodes[i++];
+		old = nodes[i++];
 		if (old !== nuNodes) {
 			if (old) this.replaceChild(nuNodes, old);
 			else this.appendChild(nuNodes);
 		}
 	}
-	while (oldNodes.length > i) this.removeChild(oldNodes[i]);
+	while (nodes.length > i) this.removeChild(nodes[i]);
 	return this;
 };
