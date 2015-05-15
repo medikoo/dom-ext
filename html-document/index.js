7f76689539b819871cd4065aca60c0f760154674
Marek Tuchowski <marek@tuchowski.com.pl>
2015-05-15 11:26:33 +0200

    Added to-id-string tests. Added to-id-string to html-document/index.js.

--- html-document/index.js
+++ html-document/index.js
@@ -3,6 +3,7 @@
 module.exports = {
 	'#':                      require('./#'),
 	isHTMLDocument:           require('./is-html-document'),
+	toIdString:               require('./to-id-string'),
 	generateId:               require('./generate-id'),
 	generateInlineScriptText: require('./generate-inline-script-text'),
 	validHTMLDocument:        require('./valid-html-document')
---
+++ test/html-document/to-id-string.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function (t, a) {
+	a(t(null), 'null');
+	a(t(undefined), 'undefined');
+	a(t('foo'), 'foo');
+	a(t('foo-bar'), 'foo-bar');
+	a(t('-bar'), 'bar');
+	a(t('mąćłńora'), 'm-ora');
+	a(t('  Some    Text  '), 'some-text');
+};
