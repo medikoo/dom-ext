91393deb0840701fb3d58cdaccd13485dd552946
Marek Tuchowski <marek@tuchowski.com.pl>
2015-05-15 10:37:21 +0200

    Moved id string generation to its own module for reusability.

--- html-document/generate-id.js
+++ html-document/generate-id.js
@@ -1,19 +1,13 @@
 'use strict';
 
-var startsWith = require('es5-ext/string/#/starts-with')
-  , endsWith   = require('es5-ext/string/#/ends-with')
-  , randomUniq = require('es5-ext/string/random-uniq')
-  , nonAsciiRe = /[\0-\/:-@\[-`{-\uffff]/g
-  , dashRe     = /-{2,}/g;
+var randomUniq = require('es5-ext/string/random-uniq')
+  , toIdString = require('./to-id-string');
 
 var generateFromName = (function () {
 	var done = Object.create(null);
 	return function (name) {
 		var count;
-		name = String(name).toLowerCase().trim().replace(nonAsciiRe, '-').replace(dashRe, '-');
-		if (startsWith.call(name, '-')) name = name.slice(1);
-		if (endsWith.call(name, '-')) name = name.slice(0, -1);
-		if (!name) name = 'x';
+		name = toIdString(name);
 		if (done[name]) {
 			count = 2;
 			while (done[name + '-' + count]) ++count;
