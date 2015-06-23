891dcf0080b9a9efe646f74a0f5b87aa5a78fd77
Mariusz Nowak <medyk@medikoo.com>
2015-06-23 12:17:07 +0200

    Add ident utilities to index

--- element/index.js
+++ element/index.js
@@ -2,6 +2,8 @@
 
 module.exports = {
 	'#':          require('./#'),
+	ensureIdent:  require('./ensure-ident'),
 	isElement:    require('./is-element'),
+	isIdent:      require('./is-ident'),
 	validElement: require('./valid-element')
 };
