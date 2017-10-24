774f04e9f2fd650b47d614378a29b3c175402730
Mariusz Nowak <medyk@medikoo.com>
2017-10-24 15:06:33 +0200

    style: prettier

--- _html-node/#/disable-controls.js
+++ _html-node/#/disable-controls.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var callable = require("es5-ext/object/valid-callable")
-  , filter   = Array.prototype.filter;
+var callable = require("es5-ext/object/valid-callable");
+
+var filter = Array.prototype.filter;
 
 module.exports = function (controlsFilter) {
 	if (controlsFilter != null) callable(controlsFilter);
-	return filter.call(this.querySelectorAll("input, textarea, select, button"),
-		function (control) {
-			if (controlsFilter && !controlsFilter(control)) return false;
-			if (control.disabled) return false;
-			control.disabled = true;
-			control.setAttribute("disabled", "disabled");
-			return true;
-		});
+	return filter.call(this.querySelectorAll("input, textarea, select, button"), function (control) {
+		if (controlsFilter && !controlsFilter(control)) return false;
+		if (control.disabled) return false;
+		control.disabled = true;
+		control.setAttribute("disabled", "disabled");
+		return true;
+	});
 };
