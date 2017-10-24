d55ecc2031c36ecd429949e041a4f272a5ff3fd2
Mariusz Nowak <medyk@medikoo.com>
2017-10-24 17:31:24 +0200

    style: prettier

--- attr/is-attr.js
+++ attr/is-attr.js
@@ -1,7 +1,12 @@
 "use strict";
 
 module.exports = function (x) {
-	return (x && (typeof x.name === "string") && (typeof x.value === "string") &&
-		((typeof x.localName === "string") || (x.localName === null)) &&
-		((x.nodeType === 2) || !x.nodeType)) || false;
+	return (
+		(x &&
+			typeof x.name === "string" &&
+			typeof x.value === "string" &&
+			(typeof x.localName === "string" || x.localName === null) &&
+			(x.nodeType === 2 || !x.nodeType)) ||
+		false
+	);
 };
