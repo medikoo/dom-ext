155af0fc323c88534f33d24007273c1a16b63684
Mariusz Nowak <medyk@medikoo.com>
2017-10-25 10:15:50 +0200

    style: prettier

--- html-element/#/dispatch-event-2.js
+++ html-element/#/dispatch-event-2.js
@@ -5,41 +5,47 @@
 
 var assign  = require("es5-ext/object/assign")
   , forEach = require("es5-ext/object/for-each")
-  , element = require("../valid-html-element")
+  , element = require("../valid-html-element");
 
-  , mouseEvents, htmlEvents, defaultOptions;
-
-mouseEvents = { click: true,
-dblclick: true,
-mousedown: true,
-mouseup: true,
+var mouseEvents = {
+	click: true,
+	dblclick: true,
+	mousedown: true,
+	mouseup: true,
 	mouseover: true,
-mousemove: true,
-mouseout: true };
-htmlEvents = { load: true,
-unload: true,
-abort: true,
-error: true,
-select: true,
+	mousemove: true,
+	mouseout: true
+};
+var htmlEvents = {
+	load: true,
+	unload: true,
+	abort: true,
+	error: true,
+	select: true,
 	change: true,
-submit: true,
-reset: true,
-focus: true,
-blur: true,
+	submit: true,
+	reset: true,
+	focus: true,
+	blur: true,
 	resize: true,
-scroll: true };
-defaultOptions = { pointerX: 0,
-pointerY: 0,
-button: 0,
-ctrlKey: false,
+	scroll: true
+};
+var defaultOptions = {
+	pointerX: 0,
+	pointerY: 0,
+	button: 0,
+	ctrlKey: false,
 	altKey: false,
-shiftKey: false,
-metaKey: false,
-bubbles: true,
-	cancelable: true };
+	shiftKey: false,
+	metaKey: false,
+	bubbles: true,
+	cancelable: true
+};
 
 module.exports = function (name/*, options */) {
-	var type, event, inputOptions = Object(arguments[1])
+	var type
+	  , event
+	  , inputOptions = Object(arguments[1])
 	  , options = assign({}, defaultOptions, inputOptions);
 	element(this);
 	if (mouseEvents.hasOwnProperty(name)) type = "MouseEvents";
@@ -50,10 +56,23 @@ module.exports = function (name/*, options */) {
 	if (type === "HTMLEvents") {
 		event.initEvent(name, options.bubbles, options.cancelable);
 	} else {
-		event.initMouseEvent(name, options.bubbles, options.cancelable,
-			this.ownerDocument.defaultView, options.button, options.pointerX,
-			options.pointerY, options.pointerX, options.pointerY, options.ctrlKey,
-			options.altKey, options.shiftKey, options.metaKey, options.button, this);
+		event.initMouseEvent(
+			name,
+			options.bubbles,
+			options.cancelable,
+			this.ownerDocument.defaultView,
+			options.button,
+			options.pointerX,
+			options.pointerY,
+			options.pointerX,
+			options.pointerY,
+			options.ctrlKey,
+			options.altKey,
+			options.shiftKey,
+			options.metaKey,
+			options.button,
+			this
+		);
 	}
 	forEach(inputOptions, function (value, name) {
 		if (defaultOptions.hasOwnProperty(name)) return;
