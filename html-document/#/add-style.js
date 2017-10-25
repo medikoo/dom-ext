d6d4bd28ac7022014b55c63b7d29c4e15af66b4a
Mariusz Nowak <medyk@medikoo.com>
2017-10-25 10:06:43 +0200

    style: prettier

--- html-document/#/add-style.js
+++ html-document/#/add-style.js
@@ -4,17 +4,18 @@ var forEach       = require("es5-ext/object/for-each")
   , isPlainObject = require("es5-ext/object/is-plain-object")
   , camelToHyphen = require("es5-ext/string/#/camel-to-hyphen")
   , memoize       = require("memoizee/plain")
-  , document      = require("../valid-html-document")
+  , document      = require("../valid-html-document");
 
-  , getStyleEl, stringify;
+var getStyleEl = memoize(
+	function (document) {
+		var style = document.head.appendChild(document.createElement("style"));
+		style.setAttribute("type", "text/css");
+		return style;
+	},
+	{ normalizer: require("memoizee/normalizers/get-1")() }
+);
 
-getStyleEl = memoize(function (document) {
-	var style = document.head.appendChild(document.createElement("style"));
-	style.setAttribute("type", "text/css");
-	return style;
-}, { normalizer: require("memoizee/normalizers/get-1")() });
-
-stringify = function (obj) {
+var stringify = function (obj) {
 	var str = [];
 	forEach(obj, function (value, key) {
 		str.push(key + " {");
