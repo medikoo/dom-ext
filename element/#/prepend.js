9d529391a8ddd329d22b785299ad3d0e2d7c43e0
Mariusz Nowak <medyk@medikoo.com>
2017-10-25 10:03:38 +0200

    style: prettier

--- element/#/prepend.js
+++ element/#/prepend.js
@@ -4,6 +4,5 @@ var element        = require("../valid-element")
   , mutationMethod = require("./_mutation-method");
 
 module.exports = function (node/*, …nodes*/) {
-	element(this).insertBefore(mutationMethod(this.ownerDocument, arguments),
-		this.firstChild);
+	element(this).insertBefore(mutationMethod(this.ownerDocument, arguments), this.firstChild);
 };
