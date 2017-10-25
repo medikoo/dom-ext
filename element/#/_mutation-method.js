a638c71af3aaf351e4d540031179e77d1e115c0a
Mariusz Nowak <medyk@medikoo.com>
2017-10-25 10:01:22 +0200

    style: prettier

--- element/#/_mutation-method.js
+++ element/#/_mutation-method.js
@@ -1,17 +1,21 @@
 "use strict";
 
-var isString = require("es5-ext/string/is-string")
+var isString = require("es5-ext/string/is-string");
 
-  , forEach = Array.prototype.forEach;
+var forEach = Array.prototype.forEach;
 
 module.exports = function (document, args) {
 	var node;
 	if (args.length > 1) {
 		node = document.createDocumentFragment();
-		forEach.call(args, function (node) {
-			if (isString(node)) node = document.createTextNode(node);
-			this.appendChild(node);
-		}, node);
+		forEach.call(
+			args,
+			function (node) {
+				if (isString(node)) node = document.createTextNode(node);
+				this.appendChild(node);
+			},
+			node
+		);
 		return node;
 	}
 	node = args[0];
